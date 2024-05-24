5b6e826491224719620b0539954d4b03cf820220
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-05-24 19:18:11 -0300

    Update SQL script.sql

--- SQL script.sql
+++ SQL script.sql
@@ -6,10 +6,11 @@ create table tbLogins(
     email varchar(200) NOT NULL,
     senha varchar(200) NOT NULL);
 
- create table tbAlunos(
+create table tbAlunos(
 	idAluno int PRIMARY KEY NOT NULL auto_increment,
     nomeCompleto varchar(200) NOT NULL,
     serie varchar(200) NOT NULL,
+    codMatricula varchar(80) NOT NULL,
     idLogin int NOT NULL,
 	FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin));
 
@@ -20,31 +21,28 @@ create table tbProfessores(
     idLogin int NOT NULL,
     FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin));
     
+create table tbVanguardas(
+	idVanguarda int PRIMARY KEY auto_increment,
+    vanguarda varchar(80));
+    
 create table tbGrupos(
 	idGrupo int PRIMARY KEY NOT NULL auto_increment, 
 	nomeGrupo varchar(200) NOT NULL, 
     qtdeAcertos int NOT NULL DEFAULT 0, 
     qtdeErros int NOT NULL DEFAULT 0,
-    tempoResolucaoVanguarda1 TIME NOT NULL DEFAULT '00:00:00',
-    tempoResolucaoVanguarda2 TIME NOT NULL DEFAULT '00:00:00',
-    tempoResolucaoVanguarda3 TIME NOT NULL DEFAULT '00:00:00',
-    tempoResolucaoVanguarda4 TIME NOT NULL DEFAULT '00:00:00',
-    tempoResolucaoVanguarda5 TIME NOT NULL DEFAULT '00:00:00',
-    tempoResolucaoVanguarda6 TIME NOT NULL DEFAULT '00:00:00');
+    tempoResolucaoVanguarda TIME NOT NULL DEFAULT '00:00:00',
+    idVanguarda int,
+    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
     
 create table integrantesGrupo(
-	idAluno int PRIMARY KEY NOT NULL,
+	idLogin int PRIMARY KEY NOT NULL,
     idGrupo int NOT NULL,
-    FOREIGN KEY (idAluno) REFERENCES tbAlunos(idAluno),
+    FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin),
     FOREIGN KEY (idGrupo) REFERENCES tbGrupos(idGrupo));
-
-create table tbVanguardas(
-	idVanguarda int PRIMARY KEY auto_increment,
-    vanguarda varchar(80));
 
 create table tbResumos(
 	idResumo int PRIMARY KEY NOT NULL auto_increment, 
-    resumo mediumtext NOT NULL,
+    resumo mediumtext,
     idVanguarda int NOT NULL, 
     FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
 
@@ -59,4 +57,10 @@ create table tbQuestoes(
     qtdeAcertos int NOT NULL default 0,
     qtdeErros int NOT NULL default 0,
     idVanguarda int NOT NULL, 
+    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
+    
+create table tbPecas(
+	idPecas int PRIMARY KEY NOT NULL auto_increment,
+    urlImagem mediumtext NOT NULL,
+    idVanguarda int NOT NULL,
     FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));

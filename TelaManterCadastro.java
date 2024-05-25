9878fe218c7d3fe587a7296394f97829be4fa1b7
CarolinaTartarotiShishito <167549701+CarolinaTartarotiShishito@users.noreply.github.com>
2024-05-25 00:08:47 -0300

    Update TelaManterCadastro.java

--- TelaManterCadastro.java
+++ TelaManterCadastro.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package com.mycompany.quizartvantgarde;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;

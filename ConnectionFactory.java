c84e53a9f814bd5a9c96ac3d6bc1d75289dbf87c
CarolinaTartarotiShishito <167549701+CarolinaTartarotiShishito@users.noreply.github.com>
2024-05-24 23:11:16 -0300

    Create ConnectionFactory.java

---
+++ ConnectionFactory.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    private static String host = "mysql-2bee4216-quizartvantgarde-803a.d.aivencloud.com";
+    private static String porta = "20445";
+    private static String db = "quizArtVantGarde";
+    private static String usuario = "avnadmin";
+    private static String senha = "AVNS_vsWw4i6D6oJ4T7iYI4J";
+    
+    public static Connection obterConexao() throws Exception {
+        System.out.println("conectando no bd");
+        String url = "jdbc:mysql://" + host + ":" + porta + "/" + db;  
+        return DriverManager.getConnection(url, usuario, senha);
+    }
+}

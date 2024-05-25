9b797e664747d688184cfde06633e09f6ada11fc
CarolinaTartarotiShishito <167549701+CarolinaTartarotiShishito@users.noreply.github.com>
2024-05-24 23:25:07 -0300

    Update DAO.java

--- DAO.java
+++ DAO.java
@@ -285,5 +285,70 @@ public class DAO {
             return ranking;
         }
     }
+    // Para pegar o resumo do banco de dados e mostrar na interface para o ator
+    public String pegarResumo (int idVanguarda) throws Exception{
+
+        String sql = "SELECT resumo FROM tbResumos WHERE idVanguarda = ?";
+        try (Connection conn = ConnectionFactory.obterConexao();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+        ps.setInt(1, idVanguarda);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return rs.getString("resumo");
+        }
+    }
+    // Método que retorna valor booleano confirmando se existe um resumo ou não
+    public boolean existeResumo(int idVanguarda) throws Exception{
+        String sql = "SELECT resumo FROM tbResumos WHERE idVanguarda = ? and resumo IS NULL";
+        try(Connection conn = ConnectionFactory.obterConexao();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+            
+            ps.setInt(1, idVanguarda);
+            try(ResultSet rs = ps.executeQuery()){
+                return rs.next();
+            }
+        }
+    }
+    // Método para adicionar no banco de dados um resumo digitado pelo ator
+    public void adicionarResumo (int idVanguarda, Resumo resumo) throws Exception{
+        System.out.println("acessei1");
+        String sql = "UPDATE tbResumos SET resumo = ? WHERE idVanguarda = ?";//errado: INSERT INTO tbResumos (resumo, idVanguarda) VALUES (?, ?)";
+        System.out.println("acessei2");
+        try (Connection conn = ConnectionFactory.obterConexao();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+            System.out.println("acessei3");
+        
+            ps.setString(1, resumo.getResumo());
+            ps.execute();
+        }
+    }
+    // Método para remover do banco de dados um resumo digitado pelo ator
+    public void removerResumo(int idVanguarda, String resumo)throws Exception{
+        String sql = "UPDATE tbResumos SET resumo = NULL WHERE idVanguarda = ?";
+        try(Connection conn = ConnectionFactory.obterConexao();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, idVanguarda);
+            ps.execute();
+        }
+    }
+    // Método para alterar no banco de dados resumo editado pelo ator
+    public void alterarResumo (int idVanguarda, String resumo) throws Exception{
+        String sql = "UPDATE tbResumos SET resumo = ? WHERE idVanguarda = ?";
+        try (Connection conn = ConnectionFactory.obterConexao();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+        ps.setString(1, resumo);
+        ps.execute();
+        }
+    }
+    // Método para ver na tabela de professores se o idLogin existe
+    public boolean existeProfessor(int idLogin) throws Exception {
+        String sql = "SELECT * FROM tbProfessores WHERE idLogin = ?";
+        try(Connection conn = ConnectionFactory.obterConexao(); PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, idLogin);
+            try(ResultSet rs = ps.executeQuery()){
+                return rs.next();
+            }
+        }
+    }
     
 }

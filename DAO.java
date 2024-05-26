a178e9873c15ed9ed04d70dfb081ea169368d523
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-05-26 20:31:16 -0300

    Update DAO.java

--- DAO.java
+++ DAO.java
@@ -92,6 +92,26 @@ public class DAO {
             ps.execute();
         }
     }
+        //Mostrar o cadastro de alunos juntamente com seu login
+    public ArrayList<CadastroAluno> pegarCadastrosAlunos() throws Exception {
+        String sql = "SELECT l.idLogin, l.email, l.senha, a.idAluno, a.nomeCompleto, a.serie, a.codMatricula FROM tbLogins l JOIN tbAlunos a ON l.idLogin = a.idLogin";
+        ArrayList<CadastroAluno> cadastrosAlunos = new ArrayList<>();
+        try(Connection conn = ConnectionFactory.obterConexao(); 
+                PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY); 
+                ResultSet rs = ps.executeQuery()){
+            while(rs.next()){
+                int idLogin = rs.getInt("l.idLogin");
+                int idAluno = rs.getInt("a.idAluno");
+                String email = rs.getString("l.email");
+                String senha = rs.getString("l.senha");
+                String nomeCompleto = rs.getString("a.nomeCompleto");
+                String serie = rs.getString("a.serie");
+                String codMatricula = rs.getString("a.codMatricula");
+                cadastrosAlunos.add(new CadastroAluno (idLogin, idAluno, email, senha, nomeCompleto, serie, codMatricula));
+            }
+            return cadastrosAlunos;
+        }
+    }
      
     //Métodos da classe Professor
         //Consultar professor
@@ -127,7 +147,7 @@ public class DAO {
         }
     }   
     public void cadastroIntegrantesGrupo(int idGrupo, int idAluno) throws Exception {
-        String sql = "INSERT INTO integrantesGrupo(idAluno, idGrupo VALUES (?, ?)";
+        String sql = "INSERT INTO integrantesGrupo(idAluno, idGrupo) VALUES (?, ?)";
         try(Connection conn = ConnectionFactory.obterConexao(); PreparedStatement ps = conn.prepareStatement(sql)){
             ps.setInt(1, idAluno);
             ps.setInt(2, idGrupo);
@@ -136,12 +156,29 @@ public class DAO {
     }
         //Contabilizar acertos e erros do grupo
     public void contabilizarAcertosErros(int idGrupo, int acertos, int erros) throws Exception {
-        String sql = "INSERT INTO tbGrupos(qtdeAcertos, qtdeErros) values(?, ?) HWERE idGrupo = ?";
+        String sql = "UPDATE tbGrupos SET qtdeAcertos = ? qtdeErros = ? WHERE idGrupo = ?";
         try(Connection conn = ConnectionFactory.obterConexao(); PreparedStatement ps = conn.prepareStatement(sql)){
             ps.setInt(1, acertos);
             ps.setInt(2, erros);
             ps.setInt(3, idGrupo);
             ps.execute();
+        }
+    }
+        //Mostrar a posição de cada grupo no ranking
+    public ArrayList<Ranking> pegarRanking(int idVanguarda) throws Exception {
+        String sql = "SELECT nomeGrupo, qtdeAcertos, qtdeErros FROM tbGrupos WHERE idVanguarda = ? ORDER BY qtdeAcertos, qtdeErros";
+        ArrayList<Ranking> ranking = new ArrayList<>();
+        try(Connection conn = ConnectionFactory.obterConexao(); 
+                PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)){
+            ps.setInt(1, idVanguarda);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                String nomeGrupo = rs.getString("nomeGrupo");
+                int qtdeAcertos = rs.getInt("qtdeAcertos");
+                int qtdeErros = rs.getInt("qtdeErros");
+                ranking.add(new Ranking (nomeGrupo, qtdeAcertos, qtdeErros));
+            }
+            return ranking;
         }
     }
     
@@ -225,9 +262,7 @@ public class DAO {
             rs.next();
             return rs.getString("respostaCorreta");
         }
-    } 
-    
-    //Métodos da classe Resumo
+    }
     
     public String pegarNomeVanguarda(int idVanguarda) throws Exception {
         String sql = "SELECT vanguarda FROM tbVanguardas WHERE idVanguarda = ?";
@@ -238,46 +273,10 @@ public class DAO {
             return rs.getString("vanguarda");
         }
     }
-    
-    public ArrayList<CadastroAluno> pegarCadastrosAlunos() throws Exception {
-        String sql = "SELECT l.idLogin, l.email, l.senha, a.idAluno, a.nomeCompleto, a.serie, a.codMatricula FROM tbLogins l JOIN tbAlunos a ON l.idLogin = a.idLogin";
-        ArrayList<CadastroAluno> cadastrosAlunos = new ArrayList<>();
-        try(Connection conn = ConnectionFactory.obterConexao(); 
-                PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY); 
-                ResultSet rs = ps.executeQuery()){
-            while(rs.next()){
-                int idLogin = rs.getInt("l.idLogin");
-                int idAluno = rs.getInt("a.idAluno");
-                String email = rs.getString("l.email");
-                String senha = rs.getString("l.senha");
-                String nomeCompleto = rs.getString("a.nomeCompleto");
-                String serie = rs.getString("a.serie");
-                String codMatricula = rs.getString("a.codMatricula");
-                cadastrosAlunos.add(new CadastroAluno (idLogin, idAluno, email, senha, nomeCompleto, serie, codMatricula));
-            }
-            return cadastrosAlunos;
-        }
-    }
-    
-    public ArrayList<Ranking> pegarRanking(int idVanguarda) throws Exception {
-        String sql = "SELECT nomeGrupo, qtdeAcertos, qtdeErros FROM tbGrupos WHERE idVanguarda = ?";
-        ArrayList<Ranking> ranking = new ArrayList<>();
-        try(Connection conn = ConnectionFactory.obterConexao(); 
-                PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)){
-            ps.setInt(1, idVanguarda);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                String nomeGrupo = rs.getString("nomeGrupo");
-                int qtdeAcertos = rs.getInt("qtdeAcertos");
-                int qtdeErros = rs.getInt("qtdeErros");
-                ranking.add(new Ranking (nomeGrupo, qtdeAcertos, qtdeErros));
-            }
-            return ranking;
-        }
-    }
-    // Para pegar o resumo do banco de dados e mostrar na interface para o ator
-    public String pegarResumo (int idVanguarda) throws Exception{
 
+    //Métodos da classe Resumo
+        // Para pegar o resumo do banco de dados e mostrar na interface para o ator
+    public String pegarResumo (int idVanguarda) throws Exception{
         String sql = "SELECT resumo FROM tbResumos WHERE idVanguarda = ?";
         try (Connection conn = ConnectionFactory.obterConexao();
             PreparedStatement ps = conn.prepareStatement(sql)){
@@ -287,7 +286,7 @@ public class DAO {
         return rs.getString("resumo");
         }
     }
-    // Método que retorna valor booleano confirmando se existe um resumo ou não
+        // Método que retorna valor booleano confirmando se existe um resumo ou não
     public boolean existeResumo(int idVanguarda) throws Exception{
         String sql = "SELECT resumo FROM tbResumos WHERE idVanguarda = ? and resumo IS NULL";
         try(Connection conn = ConnectionFactory.obterConexao();
@@ -299,29 +298,26 @@ public class DAO {
             }
         }
     }
-    // Método para adicionar no banco de dados um resumo digitado pelo ator
-    public void adicionarResumo (int idVanguarda, Resumo resumo) throws Exception{
-        System.out.println("acessei1");
+        // Método para adicionar no banco de dados um resumo digitado pelo ator
+    public void adicionarResumo (Resumo resumo) throws Exception{
         String sql = "UPDATE tbResumos SET resumo = ? WHERE idVanguarda = ?";//errado: INSERT INTO tbResumos (resumo, idVanguarda) VALUES (?, ?)";
-        System.out.println("acessei2");
         try (Connection conn = ConnectionFactory.obterConexao();
             PreparedStatement ps = conn.prepareStatement(sql)){
-            System.out.println("acessei3");
-        
             ps.setString(1, resumo.getResumo());
+            ps.setInt(2, resumo.getIdVanguarda());
             ps.execute();
         }
     }
-    // Método para remover do banco de dados um resumo digitado pelo ator
-    public void removerResumo(int idVanguarda, String resumo)throws Exception{
+        // Método para remover do banco de dados um resumo digitado pelo ator
+    public void removerResumo(Resumo resumo)throws Exception{
         String sql = "UPDATE tbResumos SET resumo = NULL WHERE idVanguarda = ?";
         try(Connection conn = ConnectionFactory.obterConexao();
         PreparedStatement ps = conn.prepareStatement(sql)){
-            ps.setInt(1, idVanguarda);
+            ps.setInt(1, resumo.getIdVanguarda);
             ps.execute();
         }
     }
-    // Método para alterar no banco de dados resumo editado pelo ator
+        // Método para alterar no banco de dados resumo editado pelo ator
     public void alterarResumo (int idVanguarda, String resumo) throws Exception{
         String sql = "UPDATE tbResumos SET resumo = ? WHERE idVanguarda = ?";
         try (Connection conn = ConnectionFactory.obterConexao();
@@ -330,7 +326,8 @@ public class DAO {
         ps.execute();
         }
     }
-    // Método para ver na tabela de professores se o idLogin existe
+    
+        // Método para ver na tabela de professores se o idLogin existe
     public boolean existeProfessor(int idLogin) throws Exception {
         String sql = "SELECT * FROM tbProfessores WHERE idLogin = ?";
         try(Connection conn = ConnectionFactory.obterConexao(); PreparedStatement ps = conn.prepareStatement(sql)){

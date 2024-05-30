7f7ef58252396ecb38eaf32bf479c11e8d47ffab
CarolinaTartarotiShishito <167549701+CarolinaTartarotiShishito@users.noreply.github.com>
2024-05-29 21:52:38 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -10,37 +10,43 @@ import javax.sound.sampled.Clip;
 public class Musica{
  
     private Clip clip;
+    String filePathField = "src\\main\\java\\wav\\Quiz.wav";
+    boolean isPaused;
     
     public void playMusic() {
         
-        String filePathField = "src\\main\\java\\wav\\Quiz.wav";
-        boolean isLooping = true;
-        
-        if (clip != null && clip.isRunning()) 
-        {
-            clip.stop();
-        }
-        
-        try 
-        {
+        try {
+            // Localização do soundtrack
             File file = new File(filePathField);
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
             
             clip = AudioSystem.getClip();
             clip.open(audioIn);
             
-            if (isLooping) 
-            {
-                clip.loop(Clip.LOOP_CONTINUOUSLY);
-            }
-            
             clip.start();
- 
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            isPaused = false;
+        }catch(Exception e){
+            System.out.println("Problemas técnicos");
         }
         
+    }
+    
+    public void pauseMusic(){
+        try{
+            if (clip != null && clip.isRunning() && isPaused == false) {            
+            // Localização do soundtrack
+            File file = new File(filePathField);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
+            
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            
+            clip.stop();
+            isPaused = true;
+            }
+        }catch(Exception e){
+            System.out.println("Problemas técnicos");
+        }
     }
 }

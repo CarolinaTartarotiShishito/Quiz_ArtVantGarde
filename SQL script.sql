a4cf6aa332ddfa69a7dcf9a4f49086224ba3dc8c
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-05-31 19:16:57 -0300

    Update SQL script.sql

--- SQL script.sql
+++ SQL script.sql
@@ -1,66 +1,69 @@
 create database quizArtVantGarde;
 use quizArtVantGarde;
 
+
 create table tbLogins(
 	idLogin int PRIMARY KEY auto_increment NOT NULL,
-    email varchar(200) NOT NULL,
-    senha varchar(200) NOT NULL);
+	email varchar(200) NOT NULL,
+	senha varchar(200) NOT NULL);
 
 create table tbAlunos(
 	idAluno int PRIMARY KEY NOT NULL auto_increment,
-    nomeCompleto varchar(200) NOT NULL,
-    serie varchar(200) NOT NULL,
-    codMatricula varchar(80) NOT NULL,
-    idLogin int NOT NULL,
+	nomeCompleto varchar(200) NOT NULL,
+	serie varchar(200) NOT NULL,
+	codMatricula varchar(80) NOT NULL,
+	idLogin int NOT NULL,
 	FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin));
 
 create table tbProfessores(
 	idProfessor int PRIMARY KEY NOT NULL auto_increment,
-    nomeCompleto varchar(200) NOT NULL,
-    materiaLecionada varchar(200) NOT NULL,
-    idLogin int NOT NULL,
-    FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin));
+	nomeCompleto varchar(200) NOT NULL,
+	materiaLecionada varchar(200) NOT NULL,
+	idLogin int NOT NULL,
+	FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin));
     
 create table tbVanguardas(
 	idVanguarda int PRIMARY KEY auto_increment,
-    vanguarda varchar(80));
+	vanguarda varchar(80));
     
 create table tbGrupos(
 	idGrupo int PRIMARY KEY NOT NULL auto_increment, 
 	nomeGrupo varchar(200) NOT NULL, 
-    qtdeAcertos int NOT NULL DEFAULT 0, 
-    qtdeErros int NOT NULL DEFAULT 0,
-    tempoResolucaoVanguarda TIME NOT NULL DEFAULT '00:00:00',
-    idVanguarda int,
-    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
-    
+	qtdeAcertos int NOT NULL DEFAULT 0, 
+	qtdeErros int NOT NULL DEFAULT 0,
+	tempoResolucaoVanguarda TIME NOT NULL DEFAULT '00:00:00',
+	idVanguarda int,
+	FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
+
 create table integrantesGrupo(
-	idLogin int PRIMARY KEY NOT NULL,
-    idGrupo int NOT NULL,
-    FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin),
-    FOREIGN KEY (idGrupo) REFERENCES tbGrupos(idGrupo));
+	idIG int PRIMARY KEY NOT NULL,
+ 	idLogin int NOT NULL,
+	idGrupo int NOT NULL,
+	FOREIGN KEY (idLogin) REFERENCES tbLogins(idLogin),
+	FOREIGN KEY (idGrupo) REFERENCES tbGrupos(idGrupo));
 
 create table tbResumos(
 	idResumo int PRIMARY KEY NOT NULL auto_increment, 
-    resumo mediumtext,
-    idVanguarda int NOT NULL, 
-    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
+	resumo mediumtext,
+	idVanguarda int NOT NULL, 
+	FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
 
 create table tbQuestoes(
 	codQuestao int PRIMARY KEY NOT NULL auto_increment,
 	pergunta mediumtext NOT NULL,
-    urlImagem mediumtext,
+    	urlImagem mediumtext,
 	respostaCorreta mediumtext NOT NULL, 
-    respostaIncorreta1 mediumtext NOT NULL, 
-    respostaIncorreta2 mediumtext NOT NULL, 
-    respostaIncorreta3 mediumtext NOT NULL,
-    qtdeAcertos int NOT NULL default 0,
-    qtdeErros int NOT NULL default 0,
-    idVanguarda int NOT NULL, 
-    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
+    	respostaIncorreta1 mediumtext NOT NULL, 
+    	respostaIncorreta2 mediumtext NOT NULL, 
+    	respostaIncorreta3 mediumtext NOT NULL,
+    	qtdeAcertos int NOT NULL default 0,
+	qtdeErros int NOT NULL default 0,
+    	idVanguarda int NOT NULL, 
+	FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
     
 create table tbPecas(
 	idPecas int PRIMARY KEY NOT NULL auto_increment,
-    urlImagem mediumtext NOT NULL,
-    idVanguarda int NOT NULL,
-    FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));
+    	urlImagem mediumtext NOT NULL,
+    	localPeca varchar(200) NOT NULL,
+    	idVanguarda int NOT NULL,
+    	FOREIGN KEY (idVanguarda) REFERENCES tbVanguardas(idVanguarda));

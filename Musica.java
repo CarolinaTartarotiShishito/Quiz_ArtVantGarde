d646c932b5f7a00c4c655eefc810eeac6ce62140
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-05-31 22:35:53 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -12,35 +12,36 @@ public class Musica{
     private Clip clip;
     String filePathField = "src\\main\\java\\wav\\Quiz.wav";
     boolean isPaused;
+    public boolean isPaused;
+    public boolean isLooping;
     
     public void playMusic() {
         
+        isLooping = true;
+        
+        if (clip != null && clip.isRunning()){
+            clip.stop();
+        }
         try {
-            // Localização do soundtrack
             File file = new File(filePathField);
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
             
             clip = AudioSystem.getClip();
             clip.open(audioIn);
-            
+            if (isLooping) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
             clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
             isPaused = false;
-        }catch(Exception e){
-            System.out.println("Problemas técnicos");
-        }
-        
+         }catch(Exception e){
+            System.out.println(e);
+         }
     }
     
     public void pauseMusic(){
         try{
             if (clip != null && clip.isRunning() && isPaused == false) {            
             // Localização do soundtrack
-            File file = new File(filePathField);
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
-            
-            clip = AudioSystem.getClip();
-            clip.open(audioIn);
             
             clip.stop();
             isPaused = true;

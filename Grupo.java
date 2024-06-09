bd24a5c8e1c49d0099f544bcafa78e809af29f22
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-06-09 20:11:48 -0300

    Update Grupo.java

--- Grupo.java
+++ Grupo.java
@@ -3,7 +3,20 @@
  * @author louis
  */
 public class Grupo {
-    public String grupo;
+    private int idGrupo;
+    private String grupo;
+    private int acertos;
+    private int erros;
+    private int tempoResolucaoVanguarda;
+    private int idVanguarda;
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
 
     public String getGrupo() {
         return grupo;
@@ -13,9 +26,49 @@ public class Grupo {
         this.grupo = grupo;
     }
 
+    public int getAcertos() {
+        return acertos;
+    }
+
+    public void setAcertos(int acertos) {
+        this.acertos = acertos;
+    }
+
+    public int getErros() {
+        return erros;
+    }
+
+    public void setErros(int erros) {
+        this.erros = erros;
+    }
+
+    public int getTempoResolucaoVanguarda() {
+        return tempoResolucaoVanguarda;
+    }
+
+    public void setTempoResolucaoVanguarda(int tempoResolucaoVanguarda) {
+        this.tempoResolucaoVanguarda = tempoResolucaoVanguarda;
+    }
+
+    public int getIdVanguarda() {
+        return idVanguarda;
+    }
+
+    public void setIdVanguarda(int idVanguarda) {
+        this.idVanguarda = idVanguarda;
+    }
+
+    public Grupo(int idGrupo, String grupo, int acertos, int erros, int tempoResolucaoVanguarda, int idVanguarda) {
+        this.idGrupo = idGrupo;
+        this.grupo = grupo;
+        this.acertos = acertos;
+        this.erros = erros;
+        this.tempoResolucaoVanguarda = tempoResolucaoVanguarda;
+        this.idVanguarda = idVanguarda;
+    }
+
     public Grupo(String grupo) {
         this.grupo = grupo;
     }
-    
     
 }

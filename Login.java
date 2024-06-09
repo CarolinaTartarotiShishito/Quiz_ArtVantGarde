3ccee9b7d80ee738252f4a67e10a33d5511ed3e4
louisebochnia <161973509+louisebochnia@users.noreply.github.com>
2024-06-09 20:12:41 -0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -3,8 +3,17 @@
  * @author louis
  */
 public class Login {
-    public String email;
-    public String senha;
+    private int idLogin;
+    private String email;
+    private String senha;
+
+    public int getIdLogin() {
+        return idLogin;
+    }
+
+    public void setIdLogin(int idLogin) {
+        this.idLogin = idLogin;
+    }
 
     public String getEmail() {
         return email;
@@ -22,6 +31,12 @@ public class Login {
         this.senha = senha;
     }
 
+    public Login(int idLogin, String email, String senha) {
+        this.idLogin = idLogin;
+        this.email = email;
+        this.senha = senha;
+    }
+    
     public Login(String email, String senha) {
         this.email = email;
         this.senha = senha;
